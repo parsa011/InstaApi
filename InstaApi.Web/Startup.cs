b4f7c3db4e15212ccf216a7929e40ba0da876e5c
parsa mahmoudy <parsa32569@gmail.com>
2020-05-22 16:50:37 +0430

    added default controller

--- InstaApi.Web/Startup.cs
+++ InstaApi.Web/Startup.cs
@@ -49,11 +49,12 @@ namespace InstaApi.Web
 
             app.UseEndpoints(endpoints =>
             {
+                endpoints.MapControllers();
                 endpoints.MapControllerRoute(name: "areas",
                     pattern: "{area:exists}/{controller=Dashboard}/{action=Index}/{id?}");
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Auth}/{action=Index}/{id?}");
             });
         }
     }
