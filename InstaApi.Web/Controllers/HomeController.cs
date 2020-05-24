2789acaad48ec431115c4f0d1c851e0b3a798ceb
parsa mahmoudy <parsa32569@gmail.com>
2020-05-24 22:47:53 +0430

    showing all images and videos simple :D

--- InstaApi.Web/Controllers/HomeController.cs
+++ InstaApi.Web/Controllers/HomeController.cs
@@ -21,6 +21,7 @@ namespace InstaApi.Web.Controllers
                 .GetDirectInboxAsync(PaginationParameters.MaxPagesToLoad(1));
             var feed = await api.FeedProcessor.GetUserTimelineFeedAsync(PaginationParameters.MaxPagesToLoad(2));
             var stories = await api.StoryProcessor.GetStoryFeedAsync();
+            
             TempData["UserName"] = (await api.GetCurrentUserAsync()).Value.UserName;
             return View(new IndexViewModel
             {
